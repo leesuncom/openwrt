51cfb859ad890362df86a87c2af26ce3026828d0
leesuncom <70786826+leesuncom@users.noreply.github.com>
2024-02-25 13:15:57 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -72,16 +72,16 @@ export rootfs_size="2560"
 export kernel_usage="stable"
 
 # 修改插件名字
-sed -i 's/"TTYD"/"终端"/g' `egrep "TTYD" -rl ./`
+sed -i 's/"TTYD"/"终端命令"/g' `egrep "TTYD" -rl ./`
 sed -i 's/"aMule设置"/"电驴下载"/g' `egrep "aMule设置" -rl ./`
 sed -i 's/"网络存储"/"NAS"/g' `egrep "网络存储" -rl ./`
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' `egrep "Turbo ACC 网络加速" -rl ./`
 sed -i 's/"实时流量监测"/"流量"/g' `egrep "实时流量监测" -rl ./`
 sed -i 's/"KMS 服务器"/"KMS激活"/g' `egrep "KMS 服务器" -rl ./`
-sed -i 's/"TTYD 终端"/"TTYD"/g' `egrep "TTYD 终端" -rl ./`
+# sed -i 's/"TTYD 终端"/"TTYD"/g' `egrep "TTYD 终端" -rl ./`
 sed -i 's/"USB 打印服务器"/"打印服务"/g' `egrep "USB 打印服务器" -rl ./`
 sed -i 's/"Web 管理"/"Web管理"/g' `egrep "Web 管理" -rl ./`
-sed -i 's/"管理权"/"改密码"/g' `egrep "管理权" -rl ./`
+sed -i 's/"管理权"/"管理密码"/g' `egrep "管理权" -rl ./`
 sed -i 's/"带宽监控"/"监控"/g' `egrep "带宽监控" -rl ./`
 sed -i 's/"Argon设置"/"主题设置"/g' `egrep "Argon设置" -rl ./`
 sed -i '6s/"OpenClash"/"世界之窗"/' `egrep "OpenClash" -rl ./`
