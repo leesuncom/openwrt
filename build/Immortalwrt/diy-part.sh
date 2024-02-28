eb4b333ce9e61ce574ec214c66c5a424f91b5e8d
leesuncom <70786826+leesuncom@users.noreply.github.com>
2024-02-28 23:12:43 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -72,6 +72,11 @@ export auto_kernel="true"
 export rootfs_size="2560"
 export kernel_usage="stable"
 
+# 回滚 iptables 为 1.8.7（1.8.8有一个致命bug，当上游升级至1.8.9时可以去除下面3行；具体参见https://www.netfilter.org/projects/iptables/files/changes-iptables-1.8.9.txt）
+grep -q "1.8.8" package/network/utils/iptables/Makefile && {
+rm -rf package/network/utils/iptables
+git_clone_path openwrt-22.03 https://github.com/openwrt/openwrt package/network/utils/iptables
+}
 
 # 修改插件名字
 sed -i 's/"TTYD"/"终端"/g' `egrep "TTYD" -rl ./`
